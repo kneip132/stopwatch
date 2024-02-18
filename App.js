632a8b9fa4aaac4020296214fb9553881fcd98f8
caseykneip <casey.kneip@coyotes.usd.edu>
2024-02-18 13:45:25 -0600

    added reset, lap, start, and stop buttons. formatted the time.

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! hi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AssetExample from './components/AssetExample';
@@ -43,4 +21,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
->>>>>>> main
--- components/AssetExample.js
+++ components/AssetExample.js
@@ -1,8 +1,10 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
 
 let runWatch = false;
 let startingTime;
 let passedTime = 0;
+
 
 function startWatch() {
     if (!runWatch) {
@@ -29,41 +31,68 @@ function displayWatch() {
     interval = setInterval(function () {
         timeChange = Date.now() - startingTime;
         showTime(timeChange);
-    })
+    });
 }
 
 function showTime(time) {
-    let millisecond = Math.floor((time%1000)/10);
-    let minute = Math.floor(time/6000);
-    let second = Math.floor((time%6000)/1000);
+  let millisecond = Math.floor((time%1000)/10);
+  let minute = Math.floor(time/6000);
+  let second = Math.floor((time%6000)/1000);
+    return `${String(minute).padStart(2, '0')}:${String(second).padStart(2, '0')}:${String(millisecond).padStart(2, '0')}`;
 }
 
 export default function AssetExample() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Hello :)
-      </Text>
-      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
-    </View>
-  );
+    const [time, setTime] = useState('00:00:00');
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.paragraph}>Stopwatch</Text>
+            <Text style={styles.timerText}>{time}</Text>
+            <View style={styles.buttonContainer}>
+                <Button title="Start" onPress={startWatch} style={styles.button} color="#00ff00"/>
+                <Button title="Lap" style={styles.button}/>
+                <Button title="Stop" onPress={stopWatch} style={styles.button} color="#ff0000"/>
+
+            </View>
+            <View style={styles.buttonContainerReset}>
+                <Button title="Reset" onPress={resetWatch} color="#696969"/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 24,
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 24,
+    },
+    paragraph: {
+      margin: 24,
+      marginTop: 0,
+      fontSize: 30,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '90%',
+      marginVertical: 10,
   },
-  paragraph: {
-    margin: 24,
-    marginTop: 0,
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
+
+  buttonContainerReset: {
+    width: '90.5%',
   },
-  logo: {
-    height: 128,
-    width: 128,
-  }
+
+  button: {
+      flex: 1,
+      width: '100%',
+  },
+    timerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
 });
